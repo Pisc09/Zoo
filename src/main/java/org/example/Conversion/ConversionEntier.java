1fd58db8ca318aac5e82047b248c31bd6016ade9
Pisco <pisco8118@gmail.com>
2025-07-07 15:29:20 +0200

    conversion chaine en entier

--- src/main/java/org/example/Conversion/ConversionEntier.java
+++ src/main/java/org/example/Conversion/ConversionEntier.java
@@ -1,4 +1,26 @@
 package org.example.Conversion;
 
+import java.util.Scanner;
+
 public class ConversionEntier {
+    public static void demanderEtConvertir() {
+        Scanner scanner = new Scanner(System.in);
+        int nombre = 0;
+        boolean saisieValide = false;
+
+        while(!saisieValide) {
+            System.out.println("entrez un nombre valide : ");
+            String saisie = scanner.nextLine();
+
+            try {
+                nombre = Integer.parseInt(saisie);
+                saisieValide = true;
+            }catch (NumberFormatException e) {
+                System.out.println("ce n'est pas un nombre entier. Veuillez réessayer : ");
+            }
+        }
+
+        System.out.println("vous avez saisie : " + nombre);
+        scanner.close();
+    }
 }
--- src/main/java/org/example/Conversion/Main.java
+++ src/main/java/org/example/Conversion/Main.java
@@ -1,4 +1,7 @@
 package org.example.Conversion;
 
 public class Main {
+    public static void main(String[] args) {
+        ConversionEntier.demanderEtConvertir();
+    }
 }
